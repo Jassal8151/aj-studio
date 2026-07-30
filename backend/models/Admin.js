e30c1155f27105c21325c9d3e4ce94dd88b1ec30
arshdeep <arshdeepsingh28482@gmail.com>
2026-07-30 10:53:40 +0530

    add images

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -22,13 +22,12 @@ const adminSchema = new mongoose.Schema(
 );
 
 // Encrypt password before save
-adminSchema.pre('save', async function (next) {
+adminSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password method
